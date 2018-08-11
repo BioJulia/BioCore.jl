66c673152c0e46e1aac37f43b5dcd612cd4590c8
Ben J. Ward <ward9250@gmail.com>
2018-08-11 16:02:00 +0100

    Update travis and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ makedocs(
 
 deploydocs(
     repo = "github.com/BioJulia/BioCore.jl.git",
-    julia = "0.6",
+    julia = "1.0",
     osname = "linux",
     target = "build",
     deps = nothing,
